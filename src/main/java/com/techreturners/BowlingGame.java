9a5b32859dd9d14aac9f7b03152c19931a6b0b91
kathyboltoncrow <kathy.bolton.crow@gmail.com>
2023-02-12 18:04:16 +0000

    all conversions passing

--- src/main/java/com/techreturners/BowlingGame.java
+++ src/main/java/com/techreturners/BowlingGame.java
@@ -4,9 +4,28 @@ public class BowlingGame {
 
    int [] rolls = new int[21];
 
-    public int[] loadGame(String input){
+    public int[] loadGame(String input) {
+        int rollsIndex = 0;
 
+        for (int i = 0; i < input.length(); i++) {
+            char roll = input.charAt(i);
+            if (roll >= '1' && roll <= '9') {
+                rolls[rollsIndex] = Integer.parseInt(String.valueOf(roll));
+                rollsIndex++;
+            } else if (roll == '-') {
+                rolls[rollsIndex] = 0;
+                rollsIndex++;
+            } else if (roll == 'X') {
+                rolls[rollsIndex] = 10;
+                rollsIndex++;
+            } else if (roll == '/') {
+                rolls[rollsIndex] = 10 - rolls[rollsIndex - 1];
+                rollsIndex++;
+            }
+
+        }
         return rolls;
     }
 
-}
+
+    }
